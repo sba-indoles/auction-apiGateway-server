cf0a3567debe4498ecbf9e9bc4c848f77a60fdb8
jay-so <merry9504@naver.com>
2024-11-07 13:52:18 +0900

    init: 프로젝트 초기 세팅

---
+++ src/test/java/org/indoles/apigatewayserver/ApigatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.indoles.apigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
